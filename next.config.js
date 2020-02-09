722318d7baf4917d6a322bb482c94aa41391f0a2
Andy Tran <hello@andytran.me>
2020-02-09 02:01:38 -0800

    feat(next): add in dotenv support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const path = require('path')
 
 module.exports = {
+  env: {
+    GHOST_API_KEY: process.env.GHOST_API_KEY,
+    GHOST_API_URL: process.env.GHOST_API_URL
+  },
   webpack (config, options) {
     config.resolve.alias.components = path.join(__dirname, 'components')
     config.resolve.alias.pages = path.join(__dirname, 'pages')
