903cf0ba4d9185e78a5afa387551b36f13bca6af
Andy Tran <hello@andytran.me>
2020-02-09 01:24:48 -0800

    feat(home-page): add in example

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Button } from 'components/Button';
 import { Card } from 'components/Card';
 
 const HomePage = () => {
 	return (
 		<Card>
 			Home page
+      <Button>View project &#10230;</Button>
 		</Card>
 	);
 };
