06096d8daeef88a22844559406ad527a24197e4f
Andy Tran <hello@andytran.me>
2020-02-09 02:38:36 -0800

    fix(next): update ghost api env name to be consistent with ghost

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const path = require('path')
 
 module.exports = {
   env: {
-    GHOST_API_KEY: process.env.GHOST_API_KEY,
-    GHOST_API_URL: process.env.GHOST_API_URL
+    GHOST_API_URL: process.env.GHOST_API_URL,
+    GHOST_ADMIN_API_KEY: process.env.GHOST_ADMIN_API_KEY,
+    GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY
   },
   webpack (config, options) {
+    config.resolve.alias.api = path.join(__dirname, 'api')
     config.resolve.alias.components = path.join(__dirname, 'components')
     config.resolve.alias.pages = path.join(__dirname, 'pages')
     config.resolve.alias.utils = path.join(__dirname, 'utils')
