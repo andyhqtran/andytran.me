0a0332d7258004f978bcd7a6036e1affe9803e7b
Andy Tran <hello@andytran.me>
2020-02-09 02:39:25 -0800

    fix(index-page): rename home to index and add in settings api

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import React, { Fragment } from 'react'
 
+import { getSettings } from 'api/settings'
 import { Button } from 'components/Button'
 import { Card } from 'components/Card'
 
-const HomePage = () => {
+const IndexPage = ({ settings }) => {
   return (
-    <Card>
-      Home page
-      <Button>View project &#10230;</Button>
-    </Card>
+    <Fragment>
+      {settings.title}
+      {settings.description}
+      <Card>
+        Index page
+        <Button>View project &#10230;</Button>
+      </Card>
+    </Fragment>
   )
 }
 
-export default HomePage
+IndexPage.propTypes = {
+  settings: PropTypes.shape({
+    description: PropTypes.string,
+    title: PropTypes.string
+  }).isRequired
+}
+
+IndexPage.getInitialProps = async () => {
+  const settings = await getSettings()
+
+  return { settings }
+}
+
+export default IndexPage
