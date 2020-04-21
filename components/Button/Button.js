3acd5abead402d671f78a3b19091f3a6282d0833
Andy Tran <hello@andytran.me>
2020-04-20 22:06:06 -0700

    fix(button): filter styled system props

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,21 +1,41 @@
+import { pick } from '@styled-system/props'
+import propTypes from '@styled-system/prop-types'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import { StyledButton } from './Button.styles'
 
-const Button = ({ children, ...restOfProps }) => {
+const Button = ({ children, className, onClick, type, ...restOfProps }) => {
   return (
-    <StyledButton {...restOfProps}>
+    <StyledButton
+      className={className}
+      onClick={onClick}
+      type={type}
+      {...pick(restOfProps)}
+    >
       {children}
     </StyledButton>
   )
 }
 
 Button.propTypes = {
+  type: 'button'
+}
+
+Button.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
-  ])
+  ]),
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  ...propTypes.background,
+  ...propTypes.color,
+  ...propTypes.flexbox,
+  ...propTypes.layout,
+  ...propTypes.position,
+  ...propTypes.space,
+  ...propTypes.typography
 }
 
 export default Button
