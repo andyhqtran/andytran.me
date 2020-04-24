8378e8c35930030d3e682c1cac3fb98dc95b775e
Andy Tran <hello@andytran.me>
2020-04-24 02:45:06 -0700

    feat(index): update hero styles and content

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
+import GhostContentAPI from '@tryghost/content-api'
 import PropTypes from 'prop-types'
 import React, { Fragment } from 'react'
 
 import { Block } from 'components/Block'
-import { Button } from 'components/Button'
-import { Card } from 'components/Card'
 import { PostCard } from 'components/PostCard'
 import { Header } from 'components/Header'
 import { Heading } from 'components/Heading'
+import { Paragraph } from 'components/Paragraph'
 
 const IndexPage = ({ description, title }) => {
   return (
     <Fragment>
       <Header />
       <Block maxWidth={1184} mx='auto' width='100%'>
-        <Heading variant='h1'>{title}</Heading>
-        {description}
+        <Block px={14} py={16}>
+          <Heading mb={6} variant='h1'>{title}</Heading>
+          <Paragraph fontSize={12} lineHeight={17}>{description}</Paragraph>
+        </Block>
         <PostCard />
       </Block>
     </Fragment>
@@ -22,10 +24,29 @@ const IndexPage = ({ description, title }) => {
 }
 
 IndexPage.propTypes = {
-  settings: PropTypes.shape({
-    description: PropTypes.string,
-    title: PropTypes.string
-  }).isRequired
+  description: PropTypes.string,
+  title: PropTypes.string
+}
+
+export async function getStaticProps () {
+  const api = new GhostContentAPI({
+    url: process.env.GHOST_API_URL,
+    key: process.env.GHOST_CONTENT_API_KEY,
+    version: 'v3'
+  })
+
+  const { description, title } = await api
+    .settings
+    .browse({
+      limit: 'all'
+    })
+
+  return {
+    props: {
+      description,
+      title
+    }
+  }
 }
 
 export default IndexPage
