8d02686f9308ca381e3e822e29ee93357cddea47
Andy Tran <hello@andytran.me>
2020-04-24 02:36:13 -0700

    feat(index): add header and postcard to index page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types'
 import React, { Fragment } from 'react'
 
+import { Block } from 'components/Block'
 import { Button } from 'components/Button'
 import { Card } from 'components/Card'
+import { PostCard } from 'components/PostCard'
+import { Header } from 'components/Header'
 import { Heading } from 'components/Heading'
 
 const IndexPage = ({ description, title }) => {
   return (
     <Fragment>
-      <Heading variant='h1'>{title}</Heading>
-      {description}
-      <Card>
-        Index page
-        <Button>View project &#10230;</Button>
-      </Card>
+      <Header />
+      <Block maxWidth={1184} mx='auto' width='100%'>
+        <Heading variant='h1'>{title}</Heading>
+        {description}
+        <PostCard />
+      </Block>
     </Fragment>
   )
 }
