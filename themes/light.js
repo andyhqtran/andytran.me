2037630382e577380dd0a2d86532bd7a84461c4b
Andy Tran <hello@andytran.me>
2020-04-24 02:53:48 -0700

    fix(light-theme): fix body color alias

--- themes/light.js
+++ themes/light.js
@@ -35,7 +35,7 @@ const colors = {
 }
 
 colors.background = colors.white // #FFFFFF
-colors.body = colors.grayscale[16] // #000000, 0.64
+colors.body = colors.grayscale[23] // #000000, 0.92
 colors.border = colors.grayscale[3] // #000000, 0.12
 colors.buttonText = colors.grayscale[25] // #000000, 1
 colors.contrast = colors.grayscale[1] // #000000, 0.04
