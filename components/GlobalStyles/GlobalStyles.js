7b815a6bdc8c4fad79eed6950aeba21c9b4037ac
Andy Tran <hello@andytran.me>
2020-04-24 02:06:51 -0700

    fix(global-styles): remove normalize

--- components/GlobalStyles/GlobalStyles.js
+++ components/GlobalStyles/GlobalStyles.js
@@ -1,13 +1,11 @@
 import { Global } from '@emotion/core'
 import { css } from '@styled-system/css'
-import emotionNormalize from 'emotion-normalize'
 import React from 'react'
 
 const GlobalStyles = () => {
   return (
     <Global
       styles={css({
-        ...emotionNormalize,
         body: {
           backgroundColor: 'background',
           color: 'body',
