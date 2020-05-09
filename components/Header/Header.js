5f8bb68ba60798b51e3c059ee1044de576361717
Andy Tran <hello@andytran.me>
2020-05-08 22:29:38 -0700

    feat(header): add in social icons to header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,17 +1,41 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import { Block } from 'components/Block'
+import { IconButton } from 'components/IconButton'
+import { CodePenIcon } from 'components/icons/CodePenIcon'
+import { GithubIcon } from 'components/icons/GithubIcon'
+import { LinkedInIcon } from 'components/icons/LinkedInIcon'
+import { TwitterIcon } from 'components/icons/TwitterIcon'
 import { Logo } from 'components/Logo'
 import { StyledHeader } from './Header.styles'
 
 const Header = ({ title }) => {
   return (
     <StyledHeader>
-      <Block maxWidth={1184} mx='auto' width='100%'>
+      <Block alignItems='center' display='flex' justifyContent='space-between' maxWidth={1184} mx='auto' width='100%'>
         <Logo title={title} />
+        <Block display='flex'>
+          <IconButton>
+            <GithubIcon />
+          </IconButton>
+          <IconButton>
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton>
+            <TwitterIcon />
+          </IconButton>
+          <IconButton>
+            <CodePenIcon />
+          </IconButton>
+        </Block>
       </Block>
     </StyledHeader>
   )
+}
+
+Header.propTypes = {
+  title: PropTypes.string
 }
 
 export default Header
