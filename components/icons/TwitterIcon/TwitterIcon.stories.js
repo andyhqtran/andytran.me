bd724716bce09012066672669c224b63d41729b6
Andy Tran <hello@andytran.me>
2020-05-08 22:02:13 -0700

    feat(twitter-icon): add in twitter icon

---
+++ components/icons/TwitterIcon/TwitterIcon.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { TwitterIcon } from 'components/icons/TwitterIcon'
+
+export default {
+  title: 'Icons/TwitterIcon',
+  component: TwitterIcon
+}
+
+export const preview = () => {
+  return (
+    <TwitterIcon />
+  )
+}
---
+++ components/icons/TwitterIcon/index.js
@@ -0,0 +1,1 @@
+export TwitterIcon from './TwitterIcon'
