baebbfcb65641e3cae396b26a9fcafa391a6553c
Andy Tran <hello@andytran.me>
2020-05-08 22:01:38 -0700

    feat(codepen-icon): add in codepen icon

---
+++ components/icons/CodePenIcon/CodePenIcon.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { CodePenIcon } from 'components/icons/CodePenIcon'
+
+export default {
+  title: 'Icons/CodePenIcon',
+  component: CodePenIcon
+}
+
+export const preview = () => {
+  return (
+    <CodePenIcon />
+  )
+}
