c0e7d8579fce35639d08b556b1d6a20fb54d0960
Andy Tran <hello@andytran.me>
2020-05-08 22:01:55 -0700

    feat(linkedin-icon): add in linkedin icon

---
+++ components/icons/LinkedInIcon/LinkedInIcon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { IconBase } from 'components/icons/IconBase'
+
+const LinkedInIcon = (props) => {
+  return (
+    <IconBase {...props}>
+      <circle cx='4.983' cy='5.009' r='2.188' />
+      <path
+        clipRule='evenodd'
+        d='M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zM3.095 8.855H6.87V20.994H3.095z'
+        fillRule='evenodd'
+      />
+    </IconBase>
+  )
+}
+
+export default LinkedInIcon
---
+++ components/icons/LinkedInIcon/index.js
@@ -0,0 +1,1 @@
+export LinkedInIcon from './LinkedInIcon'
