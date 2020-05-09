0dfd5b8d1468334471abf82e61fa8f0c8ccfdd4a
Andy Tran <hello@andytran.me>
2020-05-08 17:57:47 -0700

    docs(card): add in stories

---
+++ components/Card/Card.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Card } from 'components/Card'
+
+export default {
+  title: 'Core/Card',
+  component: Card
+}
+
+export const preview = () => {
+  return (
+    <Card height={300} />
+  )
+}
