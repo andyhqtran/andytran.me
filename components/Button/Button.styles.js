3deec503de40a30215d5fab902cf13327fd3bcad
Andy Tran <hello@andytran.me>
2020-05-08 20:39:17 -0700

    fix(button): add in hover/focus states

--- components/Button/Button.styles.js
+++ components/Button/Button.styles.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 import { css } from '@styled-system/css'
 import { themeGet } from '@styled-system/theme-get'
-import { rem } from 'polished'
+import { rem, transitions } from 'polished'
 import { background, color, compose, flexbox, layout, position, space, typography } from 'styled-system'
 
 export const StyledButton = styled('button')(
@@ -14,11 +14,15 @@ export const StyledButton = styled('button')(
     letterSpacing: rem(-0.2),
     cursor: 'pointer'
   },
+  transitions('background-color', '0.2s ease'),
   css({
     paddingLeft: 8,
     paddingRight: 8,
     color: 'buttonText',
-    fontSize: 'body'
+    fontSize: 'body',
+    '&:focus, &:hover': {
+      backgroundColor: 'contrast'
+    }
   }),
   (props) => ({
     boxShadow: `inset 0 0 0 1px ${themeGet('colors.border')(props)}`
