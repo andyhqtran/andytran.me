3d96e07ce6493281e9e82992add68cd36ab9e563
Andy Tran <hello@andytran.me>
2020-05-08 17:40:46 -0700

    fix(light-theme): reduce color options

--- themes/light.js
+++ themes/light.js
@@ -2,38 +2,21 @@ import { rgba } from 'polished'
 
 const colors = {
   black: '#000000',
-  primary: '#2B67BF',
+  primary: '#1A62CC',
   shade: [
-    rgba('#000000', 0),
     rgba('#000000', 0.04),
     rgba('#000000', 0.08),
-    rgba('#000000', 0.12),
     rgba('#000000', 0.16),
-    rgba('#000000', 0.2),
     rgba('#000000', 0.24),
-    rgba('#000000', 0.28),
-    rgba('#000000', 0.32),
-    rgba('#000000', 0.36),
     rgba('#000000', 0.4),
-    rgba('#000000', 0.44),
-    rgba('#000000', 0.48),
-    rgba('#000000', 0.52),
-    rgba('#000000', 0.56),
-    rgba('#000000', 0.6),
     rgba('#000000', 0.64),
-    rgba('#000000', 0.68),
-    rgba('#000000', 0.72),
-    rgba('#000000', 0.76),
-    rgba('#000000', 0.8),
     rgba('#000000', 0.84),
-    rgba('#000000', 0.88),
-    rgba('#000000', 0.92),
-    rgba('#000000', 0.96),
     rgba('#000000', 1)
   ],
   white: '#FFFFFF'
 }
 
+colors.shade.inverse = '#FFFFFF'
 colors.background = colors.white // #FFFFFF
 colors.body = colors.shade[23] // #000000, 0.92
 colors.border = colors.shade[3] // #000000, 0.12
