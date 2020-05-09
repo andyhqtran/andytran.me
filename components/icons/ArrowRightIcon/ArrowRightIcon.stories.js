e8849e10cf02d22ecdd17fc9f3a71d4cbb72964c
Andy Tran <hello@andytran.me>
2020-05-09 04:05:01 -0700

    feat(arrow-right-icon): add in component

---
+++ components/icons/ArrowRightIcon/ArrowRightIcon.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { ArrowRightIcon } from 'components/icons/ArrowRightIcon'
+
+export default {
+  title: 'Icons/ArrowRightIcon',
+  component: ArrowRightIcon
+}
+
+export const preview = () => {
+  return (
+    <ArrowRightIcon />
+  )
+}
---
+++ components/icons/ArrowRightIcon/index.js
@@ -0,0 +1,1 @@
+export ArrowRightIcon from './ArrowRightIcon'
