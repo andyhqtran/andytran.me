20774d7f2944558878b6ab21444765a3a6601fc5
Andy Tran <hello@andytran.me>
2020-05-09 04:22:30 -0700

    feat(motion-props): add in motion pick/omit props utils

---
+++ utils/motion-props.js
@@ -0,0 +1,56 @@
+const props = [
+  'animate',
+  'custom',
+  'drag',
+  'dragConstraints',
+  'dragElastic',
+  'dragMomentum',
+  'dragTransition',
+  'dragPropagation',
+  'exit',
+  'inherit',
+  'initial',
+  'layoutTransition',
+  'onAnimationComplete',
+  'onAnimationStart',
+  'onDrag',
+  'onDragStart',
+  'onDragEnd',
+  'onDirectionLock',
+  'onHoverStart',
+  'onHoverEnd',
+  'onPan',
+  'onPanEnd',
+  'onPanStart',
+  'onTap',
+  'onTapCancel',
+  'onTapStart',
+  'onUpdate',
+  'positionTransition',
+  'style',
+  'transformTemplate',
+  'transition',
+  'variants',
+  'whileHover',
+  'whileTap',
+];
+
+const regex = new RegExp(`^(${props.join('|')})$`)
+
+export const motionOmit = props => {
+  const next = {}
+  for (let key in props) {
+    if (regex.test(key)) continue
+    next[key] = props[key]
+  }
+  return next
+}
+
+export const motionPick = props => {
+  const next = {}
+  for (let key in props) {
+    if (!regex.test(key)) continue
+    next[key] = props[key]
+  }
+  return next
+}
