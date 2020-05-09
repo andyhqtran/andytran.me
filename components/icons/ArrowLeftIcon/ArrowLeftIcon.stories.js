fec6c2540808458838aa7d5ac552ced2d096f6b0
Andy Tran <hello@andytran.me>
2020-05-09 04:04:40 -0700

    feat(arrow-left-icon): add in component

---
+++ components/icons/ArrowLeftIcon/ArrowLeftIcon.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { ArrowLeftIcon } from 'components/icons/ArrowLeftIcon'
+
+export default {
+  title: 'Icons/ArrowLeftIcon',
+  component: ArrowLeftIcon
+}
+
+export const preview = () => {
+  return (
+    <ArrowLeftIcon />
+  )
+}
---
+++ components/icons/ArrowLeftIcon/index.js
@@ -0,0 +1,1 @@
+export ArrowLeftIcon from './ArrowLeftIcon'
