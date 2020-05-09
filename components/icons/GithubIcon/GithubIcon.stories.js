c9b55539fa1dd525a203c9c8eca5e4fd7e341306
Andy Tran <hello@andytran.me>
2020-05-08 21:46:07 -0700

    feat(github-icon): add in github icon

---
+++ components/icons/GithubIcon/GithubIcon.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { GithubIcon } from 'components/icons/GithubIcon'
+
+export default {
+  title: 'Icons/GithubIcon',
+  component: GithubIcon
+}
+
+export const preview = () => {
+  return (
+    <GithubIcon />
+  )
+}
