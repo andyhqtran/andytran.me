38c655c075a8f7bb2ea50457d1e8206e638b9dfe
Andy Tran <hello@andytran.me>
2020-05-10 03:24:21 -0700

    fix(posts-by-slug-page): add in prop-types

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React, { Fragment } from 'react'
 
 import { getPosts, getPostBySlug } from 'api/posts'
@@ -6,7 +7,6 @@ import { Block } from 'components/Block'
 import { PostTitle } from 'components/PostTitle'
 
 const PostsSlugPage = ({ post, title }) => {
-  console.log(post)
   return (
     <Fragment>
       <Block maxWidth={1184} mx='auto' width='100%'>
@@ -17,6 +17,14 @@ const PostsSlugPage = ({ post, title }) => {
       </Block>
     </Fragment>
   )
+}
+
+PostsSlugPage.propTypes = {
+  post: PropTypes.shape({
+    excerpt: PropTypes.string,
+    title: PropTypes.string
+  }),
+  title: PropTypes.string
 }
 
 export async function getStaticPaths () {
