072651f14c387c8c04608f622dff2f3432b32558
Andy Tran <hello@andytran.me>
2020-05-09 21:29:32 -0700

    feat(segment): add in segment tracking script

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,7 +1,15 @@
+import Router from 'next/router'
 import PropTypes from 'prop-types'
 import React, { Fragment } from 'react'
 
 import { Header } from 'components/Header'
+
+/**
+ * Tracks client-side page views with Segment
+ */
+Router.events.on('routeChangeComplete', url => {
+  window.analytics.page(url)
+})
 
 const Layout = ({ children, title }) => {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,12 @@
+import * as snippet from '@segment/snippet'
 import { extractCritical } from 'emotion-server'
 import Document, { Head, Main, NextScript } from 'next/document'
 import React from 'react'
+
+const {
+  SEGMENT_WRITE_KEY,
+  NODE_ENV = 'development'
+} = process.env
 
 class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
@@ -11,10 +17,26 @@ class MyDocument extends Document {
     return { ...page, ...styles }
   }
 
+  renderSnippet () {
+    const opts = {
+      apiKey: SEGMENT_WRITE_KEY,
+      page: true
+    }
+
+    if (NODE_ENV === 'development') {
+      return snippet.max(opts)
+    }
+
+    return snippet.min(opts)
+  }
+
   render () {
     return (
       <html lang='en'>
         <Head>
+          {SEGMENT_WRITE_KEY && (
+            <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
+          )}
           <style
             data-emotion-css={this.props.ids.join(' ')}
             dangerouslySetInnerHTML={{ __html: this.props.css }}
