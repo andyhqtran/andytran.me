b25f8551b3916b29eed47b7be4b5d777076b0224
Andy Tran <hello@andytran.me>
2020-05-11 04:02:39 -0700

    feat(post-card): add in stories

---
+++ components/PostCard/PostCard.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { PostCard } from 'components/PostCard'
+
+export default {
+  title: 'Post/PostCard',
+  component: PostCard
+}
+
+export const preview = () => {
+  return (
+    <PostCard
+      tags={[
+        {
+          description: 'This is a placeholder image',
+          image: 'https://via.placeholder.com/112x112/1A62CC/1A62CC',
+          title: 'Placeholder'
+        }
+      ]}
+      excerpt='Mixmax is the #1 sales engagement platform for the complete sales cycle'
+      title='Mixmax'
+    />
+  )
+}
