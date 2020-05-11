3997301178fed4b1b4233c51a0be9a50b9637f0e
Andy Tran <hello@andytran.me>
2020-05-11 04:07:13 -0700

    fix(tag): add in tag focus state

--- components/Tag/Tag.styles.js
+++ components/Tag/Tag.styles.js
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled'
 import { css } from '@styled-system/css'
+import { themeGet } from '@styled-system/theme-get'
 import { motion } from 'framer-motion'
+import { rgba, transitions } from 'polished'
 import { background, color, compose, flexbox, layout, position, space } from 'styled-system'
 export const StyledTag = styled(motion.a)(
   {
@@ -13,6 +15,12 @@ export const StyledTag = styled(motion.a)(
     cursor: 'pointer',
     userSelect: 'none'
   },
+  transitions('box-shadow', '0.2s ease'),
+  (props) => ({
+    '&:focus': {
+      boxShadow: `0 0 0 2px ${rgba(themeGet('colors.primary')(props), 0.36)}`
+    }
+  }),
   css({
     backgroundColor: 'contrast'
   }),
