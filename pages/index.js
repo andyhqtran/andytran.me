17ceb867c7b65b4140f378820510a287b4f0ddc2
Andy Tran <hello@andytran.me>
2020-05-11 03:48:18 -0700

    feat(home-page): get posts from api

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import React, { Fragment } from 'react'
 
+import { getPosts } from 'api/posts'
 import { getSettings } from 'api/settings'
 import { Block } from 'components/Block'
 import { PostCard } from 'components/PostCard'
 import { Hero } from 'components/Hero'
 
-const IndexPage = ({ description, navigation, title }) => {
+const IndexPage = ({ description, navigation, posts, title }) => {
+  console.log(posts)
   return (
     <Fragment>
       <Block maxWidth={1184} mx='auto' width='100%'>
@@ -15,7 +17,16 @@ const IndexPage = ({ description, navigation, title }) => {
           navigation={navigation}
           title={title}
         />
-        <PostCard />
+        {posts && posts.map((post) => (
+          <PostCard
+            excerpt={post.excerpt}
+            key={post.id}
+            mb={8}
+            slug={post.slug}
+            tags={post.tags}
+            title={post.title}
+          />
+        ))}
       </Block>
     </Fragment>
   )
@@ -27,16 +38,46 @@ IndexPage.propTypes = {
     label: PropTypes.string,
     url: PropTypes.string
   })),
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    excerpt: PropTypes.string,
+    image: PropTypes.string,
+    slug: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.shape({
+      description: PropTypes.string,
+      id: PropTypes.string,
+      image: PropTypes.string,
+      name: PropTypes.string,
+      slug: PropTypes.string,
+      visibility: PropTypes.string
+    })),
+    title: PropTypes.string
+  })),
   title: PropTypes.string
 }
 
 export async function getStaticProps () {
+  const posts = await getPosts({ include: 'tags,author', limit: 'all' })
   const { description, navigation, title } = await getSettings()
 
   return {
     props: {
       description,
       navigation,
+      posts: posts.map((post) => ({
+        excerpt: post.excerpt,
+        id: post.id,
+        image: post.feature_image,
+        slug: post.slug,
+        tags: post.tags && post.tags.filter((tag) => tag.visibility === 'public').map((tag) => ({
+          description: tag.description,
+          id: tag.id,
+          image: tag.feature_image,
+          name: tag.name,
+          slug: tag.slug,
+          visibility: tag.visibility
+        })),
+        title: post.title
+      })),
       title
     }
   }
