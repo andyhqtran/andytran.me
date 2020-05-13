4c679cb848a37da86b42228c1ad63a04959dfa4e
Andy Tran <hello@andytran.me>
2020-05-12 21:25:35 -0700

    style(next): fix lint error

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   env: {
     GHOST_API_URL: process.env.GHOST_API_URL,
     GHOST_ADMIN_API_KEY: process.env.GHOST_ADMIN_API_KEY,
-    GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY,
+    GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY
   },
   webpack (config, options) {
     config.resolve.alias.api = path.join(__dirname, 'api')
