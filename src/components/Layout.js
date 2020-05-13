1aa0b41e221341130646dac98eff59a5f6ecc942
Andy Tran <hello@andytran.me>
2020-05-13 00:40:22 -0700

    feat(google-analytics): add in ga support

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,15 @@ import { Header } from 'components/Header'
  * Tracks client-side page views with Segment
  */
 Router.events.on('routeChangeComplete', url => {
-  window.analytics.page(url)
+  if (window.gtag) {
+    window.gtag('config', process.env.GA_TRACKING_ID, {
+      page_path: url
+    })
+  }
+
+  if (window.analytics) {
+    window.analytics.page(url)
+  }
 })
 
 export const Layout = ({ children, navigation, postTitle, socialIcons, title }) => {
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,10 @@
 import * as snippet from '@segment/snippet'
 import { extractCritical } from 'emotion-server'
 import Document, { Head, Main, NextScript } from 'next/document'
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const {
+  GA_TRACKING_ID,
   SEGMENT_WRITE_KEY,
   NODE_ENV = 'development'
 } = process.env
@@ -34,6 +35,24 @@ class MyDocument extends Document {
     return (
       <html lang='en'>
         <Head>
+          {GA_TRACKING_ID && (
+            <Fragment>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `
+                }}
+              />
+            </Fragment>
+          )}
           {SEGMENT_WRITE_KEY && (
             <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
           )}
