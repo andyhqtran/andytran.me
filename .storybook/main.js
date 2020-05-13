90ae3f5d8d1f6bc63be5e7c53e499600a3474ada
Andy Tran <hello@andytran.me>
2020-05-12 21:29:35 -0700

    refactor(src): move src files into src folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,18 +2,19 @@ const path = require('path')
 
 module.exports = {
   stories: [
-    '../design-system/**/*.stories.[tj]s',
-    '../components/**/*.stories.[tj]s'
+    '../src/design-system/**/*.stories.[tj]s',
+    '../src/components/**/*.stories.[tj]s'
   ],
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-a11y/register'
   ],
   webpackFinal: (config) => {
-    config.resolve.alias.components = path.resolve(__dirname, '../components')
-    config.resolve.alias['design-system'] = path.resolve(__dirname, '../design-system')
-    config.resolve.alias.themes = path.resolve(__dirname, '../themes')
-    config.resolve.alias.utils = path.resolve(__dirname, '../utils')
+    config.resolve.alias.api = path.join(__dirname, '../src/api')
+    config.resolve.alias.components = path.resolve(__dirname, '../src/components')
+    config.resolve.alias['design-system'] = path.resolve(__dirname, '../src/design-system')
+    config.resolve.alias.themes = path.resolve(__dirname, '../src/themes')
+    config.resolve.alias.utils = path.resolve(__dirname, '../src/utils')
 
     return config
   }
--- next.config.js
+++ next.config.js
@@ -8,12 +8,12 @@ module.exports = {
     GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY
   },
   webpack (config, options) {
-    config.resolve.alias.api = path.join(__dirname, 'api')
-    config.resolve.alias.components = path.join(__dirname, 'components')
-    config.resolve.alias['design-system'] = path.join(__dirname, 'design-system')
-    config.resolve.alias.pages = path.join(__dirname, 'pages')
-    config.resolve.alias.themes = path.join(__dirname, 'themes')
-    config.resolve.alias.utils = path.join(__dirname, 'utils')
+    config.resolve.alias.api = path.join(__dirname, './src/api')
+    config.resolve.alias.components = path.join(__dirname, './src/components')
+    config.resolve.alias['design-system'] = path.join(__dirname, './src/design-system')
+    config.resolve.alias.pages = path.join(__dirname, './src/pages')
+    config.resolve.alias.themes = path.join(__dirname, './src/themes')
+    config.resolve.alias.utils = path.join(__dirname, './src/utils')
 
     return config
   }
