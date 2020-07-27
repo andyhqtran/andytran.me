314bda9060b47817b37eb8323bc0d513ea44f3e9
Andy Tran <hello@andytran.me>
2020-07-26 19:33:38 -0700

    chore(eslint): fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {}
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
 }
