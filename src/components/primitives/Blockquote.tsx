3611b2888dadbca935ae47a84863e8160f630415
Andy Tran <hello@andytran.me>
2020-08-08 01:32:09 -0700

    fix(blockquote): remove unused dependency

--- src/components/primitives/Blockquote.tsx
+++ src/components/primitives/Blockquote.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLProps } from 'react';
+import React from 'react';
 import { Box, BoxProps } from 'components/primitives/Box';
 
 export type BlockquoteProps = BoxProps<HTMLQuoteElement>;
