cf69fed6cc592ebfd959b3eadaf385fd52194da4
Andy Tran <hello@andytran.me>
2020-08-15 18:00:13 -0700

    refactor(post-title): update to ts

---
+++ src/components/PostTitle.tsx
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { Tag } from 'components/Tag';
+import { Box, BoxProps } from 'primitives/Box';
+import { Heading } from 'primitives/Heading';
+import { Text } from 'primitives/Text';
+
+export type PostTitleProps = BoxProps & {
+  excerpt: string;
+  tags: {
+    description: string;
+    id: string;
+    image: string;
+    name: string;
+    slug: string;
+  }[];
+  title: string;
+};
+
+export const PostTitle = ({
+  excerpt,
+  tags,
+  title,
+  ...restOfProps
+}: PostTitleProps) => {
+  const numberOfTags = tags.length;
+
+  return (
+    <Box sx={{ maxWidth: 1072, mx: 'auto', pt: 20 }} {...restOfProps}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Box sx={{ display: 'flex', mb: 24 }}>
+        {tags?.map((tag, index) => {
+          const isLastTag = numberOfTags === index + 1;
+
+          return (
+            <Tag
+              description={tag.description}
+              image={tag.image}
+              key={tag.id}
+              name={tag.name}
+              slug={tag.slug}
+              sx={{ mr: isLastTag ? 0 : 16 }}
+            />
+          );
+        })}
+      </Box>
+      <Heading sx={{ maxWidth: 704, mb: 16 }} variant='h1'>
+        {title}
+      </Heading>
+      <Text
+        sx={{
+          maxWidth: 704,
+          color: 'shade.5',
+          fontSize: 24,
+          lineHeight: '34px',
+        }}
+      >
+        {excerpt}
+      </Text>
+    </Box>
+  );
+};
