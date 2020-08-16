d5ea4621349356325945bb14f7cb397d1a632f57
Andy Tran <hello@andytran.me>
2020-08-16 04:08:53 -0700

    feat(anchor): add in outline state to anchor component

--- src/components/primitives/Anchor.tsx
+++ src/components/primitives/Anchor.tsx
@@ -1,5 +1,5 @@
 import { MotionProps, motion } from 'framer-motion';
-import { transitions } from 'polished';
+import { rgba, transitions } from 'polished';
 import React from 'react';
 
 import { Box, BoxProps } from 'primitives/Box';
@@ -12,13 +12,21 @@ export const Anchor = (props: AnchorProps) => {
       as={motion.a}
       {...props}
       __css={{
+        borderRadius: 4,
         color: 'shade.7',
         fontSize: 18,
         letterSpacing: 0.1,
         lineHeight: '28px',
-        ...transitions(['color'], '0.2s ease'),
+        ...transitions(['color', 'box-shadow'], '0.2s ease'),
         '&:hover': {
           color: 'primary',
+        },
+        '&:focus': {
+          outline: 'none',
+        },
+        '&.focus-visible': {
+          color: 'primary',
+          boxShadow: ({ colors }) => `0 0 0 2px ${rgba(colors.primary, 0.36)}`,
         },
       }}
     />
