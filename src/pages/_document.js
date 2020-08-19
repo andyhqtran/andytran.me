8c3b5147d63bb8f14ae50205812845689d415270
Andy Tran <hello@andytran.me>
2020-08-19 01:44:17 -0700

    fix(css-vars): move css vars back to document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -65,6 +65,37 @@ export default class MyDocument extends Document {
             data-emotion-css={this.props.ids.join(' ')}
             dangerouslySetInnerHTML={{ __html: this.props.css }}
           />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+                body.light-mode {
+                  --color-inverse: #FFFFFF;
+                  --color-tint-0: rgba(0, 0, 0, 0);
+                  --color-tint-1: rgba(0, 0, 0, 0.04);
+                  --color-tint-2: rgba(0, 0, 0, 0.08);
+                  --color-tint-3: rgba(0, 0, 0, 0.16);
+                  --color-tint-4: rgba(0, 0, 0, 0.24);
+                  --color-tint-5: rgba(0, 0, 0, 0.4);
+                  --color-tint-6: rgba(0, 0, 0, 0.64);
+                  --color-tint-7: rgba(0, 0, 0, 0.84);
+                  --color-tint-8: rgba(0, 0, 0, 1);
+                }
+
+                body.dark-mode {
+                  --color-inverse: #101419;
+                  --color-tint-0: rgba(255, 255, 255, 0);
+                  --color-tint-1: rgba(255, 255, 255, 0.04);
+                  --color-tint-2: rgba(255, 255, 255, 0.08);
+                  --color-tint-3: rgba(255, 255, 255, 0.16);
+                  --color-tint-4: rgba(255, 255, 255, 0.24);
+                  --color-tint-5: rgba(255, 255, 255, 0.4);
+                  --color-tint-6: rgba(255, 255, 255, 0.64);
+                  --color-tint-7: rgba(255, 255, 255, 0.84);
+                  --color-tint-8: rgba(255, 255, 255, 1);
+                }
+              `,
+            }}
+          />
         </Head>
         <body>
           <script src='/js/noflash.js' />
