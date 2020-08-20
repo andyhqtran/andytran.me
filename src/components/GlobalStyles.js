c0b6c33e7ad8d2bf1295d60e6d700ffcf8456f04
Andy Tran <hello@andytran.me>
2020-08-19 19:20:24 -0700

    fix(global-styles): remove background styles from global styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -8,7 +8,6 @@ export const GlobalStyles = () => {
     <Global
       styles={css({
         body: {
-          backgroundColor: 'inverse',
           color: 'tint7',
           fontFamily: 'copy',
           fontWeight: 'regular',
