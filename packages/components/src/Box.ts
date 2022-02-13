63c38e36ba575906650cf9019af3a3501e31424c
Andy Tran <hello@andytran.me>
2022-02-13 01:28:34 -0800

    add in components package

---
+++ packages/components/src/Box.ts
@@ -0,0 +1,6 @@
+import { styled } from '~/stitches.config';
+
+export const Box = styled('div', {
+  margin: 0,
+  boxSizing: 'border-box',
+});
---
+++ packages/components/src/index.ts
@@ -0,0 +1,3 @@
+export * from '~/stitches.config';
+
+export * from '~/Box';
