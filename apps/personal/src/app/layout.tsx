d8b905c8af33bad7830e20a6bc125469bf728bac
Andy Tran <hello@andytran.me>
2022-11-19 03:51:50 -0800

    add inter font

--- apps/personal/src/app/layout.tsx
+++ apps/personal/src/app/layout.tsx
@@ -1,6 +1,8 @@
 'use client';
 import './globals.css';
 
+import { Inter } from '@next/font/google';
+import classNames from 'classnames';
 import { NextSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
 import type { ReactNode } from 'react';
@@ -9,9 +11,11 @@ import { AnalyticsWrapper } from '~/components/AnalyticsWrapper';
 import { Footer } from '~/components/Footer';
 import { NEXT_SEO_DEFAULTS } from '~/constants/seo';
 
+const inter = Inter();
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html className='antialiased' lang='en'>
+    <html className={classNames('antialiased', inter.className)} lang='en'>
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <NextSeo {...NEXT_SEO_DEFAULTS} themeColor='#0091FF' useAppDir={true} />
