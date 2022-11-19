6b74a9a96404d6cae5f49512a3a44230bfb7d24c
Andy Tran <hello@andytran.me>
2022-11-19 01:34:51 -0800

    move to project folder

--- apps/personal/src/app/project/layout.tsx
+++ apps/personal/src/app/project/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 import { Header } from '~/components/Header';
-import { ProjectHeader } from '~/components/ProjectHeader';
+import { ProjectHeader } from '~/components/project/ProjectHeader';
 
 export default function ProjectLayout({ children }: { children: ReactNode }) {
   return (
