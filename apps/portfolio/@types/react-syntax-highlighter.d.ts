67e6e2fe47fd38a5c5d5ae5fa4ed638774fa5933
Andy Tran <hello@andytran.me>
2022-11-19 03:27:24 -0800

    remove old portfolio

--- apps/portfolio/@types/react-syntax-highlighter.d.ts
+++
@@ -1,6 +0,0 @@
-import 'react-syntax-highlighter/dist/cjs/styles/hljs';
-
-declare module 'react-syntax-highlighter/dist/cjs/styles/hljs' {
-  export { default as a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs/a11yDark';
-  export { default as a11yLight } from 'react-syntax-highlighter/dist/cjs/styles/hljs/a11yLight';
-}
--- apps/portfolio/src/pages/api/validation.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    liq: true,
-  });
-};
