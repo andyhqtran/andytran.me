bcb4a49a4bcd3da374cdda61660e38dd31ad1e87
Andy Tran <hello@andytran.me>
2022-11-20 05:28:18 -0800

    fix next-seo

--- apps/personal/src/app/layout.tsx
+++ apps/personal/src/app/layout.tsx
@@ -9,7 +9,6 @@ import type { ReactNode } from 'react';
 
 import { AnalyticsWrapper } from '~/components/AnalyticsWrapper';
 import { Footer } from '~/components/Footer';
-import { NEXT_SEO_DEFAULTS } from '~/constants/seo';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html className={classNames('antialiased', inter.className)} lang='en'>
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <NextSeo {...NEXT_SEO_DEFAULTS} themeColor='#0091FF' useAppDir={true} />
+        <NextSeo themeColor='#0091FF' useAppDir={true} />
       </head>
       <body className='bg-slate-1 selection:bg-blue-9'>
         <ThemeProvider attribute='data-theme'>
