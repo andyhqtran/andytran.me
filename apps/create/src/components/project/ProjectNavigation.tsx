cad6bc1b7e3d717657150aee7ce73e4ab7be450b
Andy Tran <hello@andytran.me>
2023-03-14 00:36:20 -0700

    add in project navigation

--- apps/create/src/components/project/ProjectNavigation.tsx
+++ apps/create/src/components/project/ProjectNavigation.tsx
@@ -1,3 +1,28 @@
+import { Component1Icon, FileIcon, GearIcon, ImageIcon, LayersIcon, TokensIcon } from '@radix-ui/react-icons';
+
+import { ProjectNavigationItem } from '~/components/project/ProjectNavigationItem';
+
 export const ProjectNavigation = () => {
-  return <nav className='h-full w-16 shrink-0 border-r border-r-slate-4 bg-slate-1' />;
+  return (
+    <nav className='flex h-full w-16 shrink-0 flex-col gap-3 border-r border-r-slate-4 bg-slate-1 p-3'>
+      <ProjectNavigationItem isActive>
+        <LayersIcon />
+      </ProjectNavigationItem>
+      <ProjectNavigationItem>
+        <FileIcon />
+      </ProjectNavigationItem>
+      <ProjectNavigationItem>
+        <Component1Icon />
+      </ProjectNavigationItem>
+      <ProjectNavigationItem>
+        <TokensIcon />
+      </ProjectNavigationItem>
+      <ProjectNavigationItem>
+        <ImageIcon />
+      </ProjectNavigationItem>
+      <ProjectNavigationItem>
+        <GearIcon />
+      </ProjectNavigationItem>
+    </nav>
+  );
 };
---
+++ apps/create/src/components/project/ProjectNavigationItem.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import { ComponentProps } from 'react';
+
+export type ProjectNavigationItemProps = ComponentProps<'button'> & {
+  isActive?: boolean;
+};
+
+export const ProjectNavigationItem = ({ children, isActive }: ProjectNavigationItemProps) => {
+  return (
+    <button
+      className={classNames(
+        'flex h-10 w-10 items-center justify-center rounded border transition-colors',
+        isActive
+          ? 'border-blue-6 bg-blue-3 text-blue-9 hover:border-blue-7 hover:bg-blue-4 hover:text-blue-10'
+          : 'border-transparent bg-slate-1 text-slate-11 hover:border-slate-5 hover:bg-slate-2 hover:text-slate-12',
+      )}
+    >
+      {children}
+    </button>
+  );
+};
