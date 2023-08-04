2afab3ee4de1d7581327aebeb8b5459f7b417b17
Andy Tran <hello@andytran.me>
2023-08-04 00:30:09 -0700

    add in coming soon message

--- apps/project-dfo/src/app/page.tsx
+++ apps/project-dfo/src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return null;
+  return (
+    <div className='flex flex-col gap-8'>
+      <div className='flex flex-col gap-1 pb-6 pt-10'>
+        <h1 className='text-2xl text-slate-12'>DFO.gg</h1>
+        <p className='text-base text-slate-11'>This tool is coming soon.</p>
+      </div>
+    </div>
+  );
 }
