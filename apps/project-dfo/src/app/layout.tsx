d0f0f9b3833f405f695d3251428e660117610624
Andy Tran <hello@andytran.me>
2023-08-07 23:22:51 -0700

    add in metadata

--- apps/project-dfo/src/app/layout.tsx
+++ apps/project-dfo/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { Metadata } from 'next';
 
 import { Providers } from '~/app/providers';
 import { Footer } from '~/components/Footer';
@@ -22,3 +23,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Dungeon Fighter Online Resources',
+  description:
+    'Unlock the ultimate Dungeon Fighter Online advantage with our extensive resources, detailed stats, and current rankings. Master the art of gameplay with strategic insights and guides. Rise to the top of the DFO world – start your journey now!',
+};
