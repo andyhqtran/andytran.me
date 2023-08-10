dce3aceb0cb824d91d19a108f9c35cb7c53f1331
Andy Tran <hello@andytran.me>
2023-08-10 03:58:43 -0700

    fix error

--- apps/project-dfo/src/app/character/[serverId]/[characterName]/page.tsx
+++ apps/project-dfo/src/app/character/[serverId]/[characterName]/page.tsx
@@ -53,7 +53,10 @@ export default async function Page({ params }: PageProps) {
       </div>
 
       <div className='flex flex-col gap-6'>
-        <EquipmentCard characterId={character.characterId} serverId={params.serverId} />
+        <EquipmentCard
+          characterId={character.characterId}
+          serverId={params.serverId === 'sirocco' ? 'siroco' : 'cain'}
+        />
 
         <CollapsibleCard title='Buff'>No buff entries.</CollapsibleCard>
 
--- apps/project-dfo/src/components/character/equipment-card/EquipmentCard.tsx
+++ apps/project-dfo/src/components/character/equipment-card/EquipmentCard.tsx
@@ -9,7 +9,7 @@ export const EquipmentCard = async ({ characterId, serverId }) => {
   return (
     <CollapsibleCard defaultOpen title='Equipment'>
       <div className='flex flex-col'>
-        {equipment.map((equip, index) => (
+        {equipment?.map((equip, index) => (
           <EquipmentCardItem equip={equip} key={index} />
         ))}
       </div>
