291caf24a28494ed5757e9b84fd71287670274fa
Andy Tran <hello@andytran.me>
2023-08-13 13:17:10 -0700

    add in null state and revert strict config

--- apps/project-dfo/src/app/rankings/[jobGrowSlug]/page.tsx
+++ apps/project-dfo/src/app/rankings/[jobGrowSlug]/page.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+
+import { Routes } from '~/constants/Routes';
 import { getJobs } from '~/data/getJobs';
 
 export type PageProps = {
@@ -10,6 +13,23 @@ export default async function Page({ params }: PageProps) {
   const jobs = await getJobs();
   const job = jobs.find((job) => job.jobGrow.find((jobGrow) => jobGrow.jobGrowSlug === params.jobGrowSlug));
   const jobGrow = job?.jobGrow.find((jobGrow) => jobGrow.jobGrowSlug === params.jobGrowSlug);
+
+  if (!jobGrow) {
+    return (
+      <div className='flex flex-col'>
+        <div className='mb-8 flex flex-col gap-3 pb-6 pt-10'>
+          <div className='flex flex-col gap-1'>
+            <h1 className='text-2xl text-slate-12'>Rankings not found</h1>
+
+            <p className='text-base text-slate-11'>
+              The rankings you&apos;re looking for doesn&apos;t exist.&nbsp;
+              <Link href={Routes.Rankings}>Click here</Link> to see all the available rankings.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className='relative z-0 flex flex-col'>
@@ -30,6 +50,12 @@ export async function generateMetadata({ params }: PageProps) {
   const jobs = await getJobs();
   const job = jobs.find((job) => job.jobGrow.find((jobGrow) => jobGrow.jobGrowSlug === params.jobGrowSlug));
   const jobGrow = job?.jobGrow.find((jobGrow) => jobGrow.jobGrowSlug === params.jobGrowSlug);
+
+  if (!jobGrow) {
+    return {
+      title: 'Rankings not found | Dungeon Fighter Online Resources',
+    };
+  }
 
   return {
     title: `${jobGrow?.jobGrowName} Rankings | Dungeon Fighter Online Resources`,
