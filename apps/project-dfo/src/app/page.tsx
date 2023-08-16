25f2753aa3e0340852144bfc768f9d4b50bdc3a4
Andy Tran <hello@andytran.me>
2023-08-16 00:20:40 -0700

    use site name env

--- apps/project-dfo/src/app/page.tsx
+++ apps/project-dfo/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <div className='flex flex-col gap-8'>
       <div className='flex flex-col gap-1 pb-6 pt-10'>
-        <h1 className='text-2xl text-slate-12'>DFO.gg</h1>
+        <h1 className='text-2xl text-slate-12'>{process.env.NEXT_PUBLIC_SITE_NAME}</h1>
         <p className='text-base text-slate-11'>This tool is coming soon.</p>
       </div>
 
