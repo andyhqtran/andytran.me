c4e3ae1329c4de43418ce95c6a70a1d44840cbad
Andy Tran <hello@andytran.me>
2023-08-17 20:05:01 -0700

    update hero copy

--- apps/project-dfo/src/app/page.tsx
+++ apps/project-dfo/src/app/page.tsx
@@ -1,14 +1,19 @@
 import { type Metadata } from 'next';
+import Link from 'next/link';
 
 import { Changelog } from '~/components/Changelog';
 import { Roadmap } from '~/components/Roadmap';
+import { ExternalRoutes } from '~/constants/Routes';
 
 export default function Page() {
   return (
     <div className='flex flex-col gap-8'>
       <div className='flex flex-col gap-1 pb-6 pt-10'>
         <h1 className='text-2xl text-slate-12'>{process.env.NEXT_PUBLIC_SITE_NAME}</h1>
-        <p className='text-base text-slate-11'>This tool is coming soon.</p>
+        <p className='text-base text-slate-11'>
+          This website is still a work-in-progress, please report any bugs on{' '}
+          <Link href={ExternalRoutes.Discord}>Discord</Link>.
+        </p>
       </div>
 
       <Changelog />
