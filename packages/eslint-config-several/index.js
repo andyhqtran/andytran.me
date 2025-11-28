d2d5614863f7369ae5158540c93acee88c6cf501
Andy Tran <hello@andytran.me>
2025-11-27 19:10:35 -0800

    chore: remove prettier

--- packages/eslint-config-several/index.js
+++ packages/eslint-config-several/index.js
@@ -6,7 +6,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:tailwindcss/recommended',
-    'prettier',
   ],
 
   parser: '@typescript-eslint/parser',
